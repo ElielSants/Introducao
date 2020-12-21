77edb711177a44dfdbb258f2b75b863b404c339a
Eliel Santos <elieltsants@gmail.com>
2020-12-21 13:32:47 -0300

    Primeira Migration

--- Curso/Domain/Cliente.cs
+++ Curso/Domain/Cliente.cs
@@ -1,10 +1,10 @@
+
 namespace Curso.Domain
 {
     public class Cliente
     {
         public int Id { get; set; }
         public string Nome { get; set; }
-        
         public string Telefone { get; set; }
         public string CEP { get; set; }
         public string Estado { get; set; }
